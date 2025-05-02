12be576e510e057fab2d3073444b7b3780ee840c
rongtao zhang <afzrt666@gmail.com>
2025-05-02 14:54:14 +0800

    闲的没事交一下

---
+++ study/src/main/java/org/example/s250502/ChangeChar.java
@@ -0,0 +1,9 @@
+package org.example.s250502;
+
+public class ChangeChar {
+    public static void main(String[] args) {
+        System.out.println("java\t Test");
+        System.out.println("书名\t 作者\t 价格\t 销量");
+        System.out.println("三国\t 罗贯中\t 120\t 1000");
+    }
+}
