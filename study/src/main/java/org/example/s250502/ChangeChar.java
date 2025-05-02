a9fa1e0043b5a3f9ab564e7ff6835e576e1052a7
rongtao zhang <afzrt666@gmail.com>
2025-05-03 02:47:27 +0800

    250502单日陈果

--- study/src/main/java/org/example/s250502/ChangeChar.java
+++ study/src/main/java/org/example/s250502/ChangeChar.java
@@ -1,9 +1,16 @@
 package org.example.s250502;
 
+/**
+ * @author rongtaozhang
+ * @version 1.0
+ * @description 练习
+ */
 public class ChangeChar {
     public static void main(String[] args) {
-        System.out.println("java\t Test");
-        System.out.println("书名\t 作者\t 价格\t 销量");
-        System.out.println("三国\t 罗贯中\t 120\t 1000");
+//        System.out.println("java\t Test");
+//        System.out.println("书名\t 作者\t 价格\t 销量");
+//        System.out.println("三国\t 罗贯中\t 120\t 1000");
+//        \t 对称 \n 换行
+        System.out.println("书名\t作者\t价格\t销量\n三国\t罗贯中\t120\t1000");
     }
 }
